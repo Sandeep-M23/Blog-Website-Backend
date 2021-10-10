dfea826cd1abf0fefb4ba8fafd0350eb37de55f4
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-10-10 18:36:23 +0530

    Replaced Database Logic

--- Controllers/Blog-controllers.js
+++ Controllers/Blog-controllers.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const {validationResult} = require('express-validator')
+const mongoose = require("mongoose");
+const {validationResult} = require('express-validator');
 
 const HttpError = require('../Models/http-Error');
+const Blog = require('../Models/blog');
 
 let DUMMY_BLOGS = [
   {
@@ -25,19 +27,29 @@ const getAllBlogs =  (req, res, next) => {
   res.json({ blogs: DUMMY_BLOGS });
 };
 
-const getBlogsById = (req, res, next) => {
-  console.log("GET BLOGS BY ID");
+const getBlogsById = async(req, res, next) => {
   const blogId = req.params.pid;
-  let blog;
-  blog = DUMMY_BLOGS.find((p) => {
-    return p.id === blogId;
-  });
 
-  if (!blog) {
-    throw new HttpError("Could not find the blog for the provided Id.", 404);
+  let blog;
+  try {
+    blog = await Blog.findById(blogId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something Went Wrong ,could not Find Blog",
+      500
+    );
+    return next(error);
   }
 
-  res.json({ blog });
+  if (!blog) {
+    const error = new HttpError(
+      "Could not find the blog for the provided Id.",
+      404
+    );
+    return next(error);
+  }
+
+  res.json({ blog: blog.toObject({ getters: true }) });
 };
 
 const getBlogsByUserId = (req, res, next) => {
@@ -57,22 +69,30 @@ const getBlogsByUserId = (req, res, next) => {
   res.json({ blogs });
 };
 
-const createBlog = (req, res, next) => {
-  console.log("POST A NEW BLOG");
+const createBlog = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
-  
+
   const { title, description, creatorId } = req.body;
 
-  const createdBlog = {
+  const createdBlog = new Blog({
     title,
     description,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvo01uSr4X5jWbAu1-wZ6zy23mKrEMtv8MFA&usqp=CAU",
     creatorId,
-  };
+  });
 
-  DUMMY_BLOGS.push(createdBlog);
+  try {
+    await createdBlog.save();
+  } catch (err) {
+    const error = new HttpError("Creating Blog Failed! Please Try Again.", 500);
+    return next(error);
+  }
 
   res.status(201).json({ blog: createdBlog });
 };
