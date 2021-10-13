4cc83242328ecbab886370414eb59d092577a166
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-10-13 20:43:35 +0530

    Connecting Blogs With UserIds

--- Models/blog.js
+++ Models/blog.js
@@ -6,7 +6,7 @@ const blogSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
-  creatorId: { type: String, required: true },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
 module.exports = mongoose.model("Blog", blogSchema);
--- Models/user.js
+++ Models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
@@ -7,9 +7,9 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
-  blogs: { type: String, required: true },
+  blogs: [{ type: mongoose.Types.ObjectId, required: true, ref: "Blog" }]
 });
 
-userSchema.plugin(uniqueValidator)
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
